5a606cd8b1dec3841cbf48d43aa8da4f188c1737
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-04-19 19:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//your JS code here. If required.
 let currentActive = 1;
 const circles = document.querySelectorAll('.circle');
+const lines = document.querySelectorAll('.line');
 const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('prev');
 
@@ -29,12 +29,18 @@ function update() {
         }
     });
 
+    lines.forEach((line, idx) => {
+        if(idx < currentActive - 1) {
+            line.classList.add('active');
+        } else {
+            line.classList.remove('active');
+        }
+    });
+
     if(currentActive === 1) {
         prevButton.disabled = true;
     } else if(currentActive === circles.length) {
         nextButton.disabled = true;
     } else {
         prevButton.disabled = false;
-        nextButton.disabled = false;
-    }
-}
+        nextButton.disabled =
